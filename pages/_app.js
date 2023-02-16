dacf897328f4b6145d67e1ca2eb2c3c9d141a453
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-02-16 16:12:46 +0100

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Cabin } from "@next/font/google";
+
+const cabin = Cabin({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <main className={cabin.className}>
+                <div className=' wrapper bg-color-main'>
+                    <Component {...pageProps} />
+                </div>
+            </main>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "color-main": "#FEFAE0",
+                "color-light": "#FAEDCD",
+                "color-brown": "#D4A373",
+                "color-l-green": "#E9EDC9",
+                "color-d-green": "#CCD5AE",
+            },
+            screens: {
+                xxs: "375px",
+                xs: "425px",
+            },
+        },
+    },
+    plugins: [],
+};
